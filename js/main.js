c2a800d0c74565df6736d2448085662590fcb456
Criptonauta <klurbidi@protonmail.com>
2025-08-03 19:38:24 +0200

    123

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById('nav-toggle').addEventListener('click', function() {
+  var menu = document.getElementById('nav-menu');
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+  } else {
+    menu.classList.add('hidden');
+  }
+});
+
